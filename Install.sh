957bcdd4e3e729c4a914250f3921c0f617e520bf
iSn0we <tombston3wrq@gmail.com>
2018-12-03 18:41:52 +0000

    gang2

---
+++ Install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "Welcome to the OrkDrone installation menu."
+echo "checking if tcpserver command is aviable..."
+if which tcpserver >/dev/null ; then
+	echo -e "[CHECK]\tTCPServer Command working already\t\t(ucspi-tcp)"
+else
+	echo -e "[WARN]\tInstalling TCPServer..."
+	sudo apt-get install ucspi-tcp --yes
+fi
+echo -e "[CHECK]\tInstalling and enabling the SystemService\t(Telnet)"
+if [ ! -f /etc/systemd/system/DroneTelnet.service ]; then
+	sudo cp System-Services/DroneTelnet.service /etc/systemd/system/
+	sudo systemctl enable DroneTelnet.service
+	echo -e "[CHECK]\tTelnet setten up on port 1337"
+else
+	echo -e "[CHECK]\tTelnet seems to be setten up already."
+fi
---
+++ SSH_Server.sh
@@ -0,0 +1,2 @@
+echo "[ORKDRONE] Starting OrkDrone CommandLine Server..."
+
